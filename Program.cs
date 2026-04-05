cd0b231b42f17306f6bd919edb85d75d5d53d006
Ishita <33433097+ibahuguna@users.noreply.github.com>
2026-04-05 18:39:09 +1000

    Created TaskItem model

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using TaskManager;
+
+List<TaskItem> tasks = new List<TaskItem>();
+TaskItem item = null;
+
+bool cont = true;
+
+while (cont)
+{
+    Console.WriteLine("Please choose an option : ");
+    Console.WriteLine("1. Add Task");
+    Console.WriteLine("2. View Tasks");
+    Console.WriteLine("3. Mark Complete");
+    Console.WriteLine("4. Exit");
+    String option = Console.ReadLine();
+    switch (option)
+    {
+        case "1":
+            // Add task code
+            break;
+        case "2":
+            //View tasks code
+            break;
+        case "3":
+            //Mark complete code
+            break;
+        case "4":
+            cont = false;
+            break;
+        default:
+            Console.WriteLine("Invalid option!");
+            break;
+    }
+}
---
+++ TaskItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager
+{
+    internal class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public void AddTask()
+        {
+
+        }
+
+        public void ViewTasks()
+        {
+
+        }
+
+        public void MarkComplete()
+        {
+
+        }
+    }
+}
